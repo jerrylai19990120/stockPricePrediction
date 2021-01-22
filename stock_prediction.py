d491a1c459e49e870036043a9c334d5f6ab12369
jerry <jerrylai19990120@gmail.com>
2021-01-21 19:48:12 -0500

    update

--- stock_prediction.py
+++ stock_prediction.py
@@ -9,15 +9,16 @@ df.index.name = "Date"
 df.drop(columns='Date', inplace=True)
 df['Price_up'] = np.where(df['Close'].shift(-1) > df['Close'], 1, 0)
 
-x = df.iloc[:, 0:df.shape[1]-1].values
-y = df.iloc[:, df.shape[1]-1].values
+x = df.iloc[:, 0:7].values
+y = df.iloc[:, 7].values
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
 
 model = DecisionTreeClassifier()
 model.fit(x_train, y_train)
 
-result = model.score(x_test, y_test)
+report= model.score(x_test, y_test)
 
-
-print(df)
+result = model.predict(x_test)
+print(report)
+print(result)
